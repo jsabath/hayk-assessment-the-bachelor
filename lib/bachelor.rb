2f7422604d0f262ef99c4bc1353b2d9c4acab29f
jsabath <justin.sabath@aol.com>
2020-02-21 15:55:08 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,40 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find do |contestant|
+    contestant["status"] == "Winner"
+    return contestant["name"].split(' ')[0] 
+end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.values.flatten.find do |contestant|
+    if contestant["occupation"] == occupation
+    return contestant["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.select {|contestant|contestant["hometown"]==hometown}.count
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.values.flatten.find do |contestant|
+    if contestant["hometown"] == hometown
+      return contestant["occupation"]
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
-end
+  age = 0
+  contestants = 0
+  data[season].each do |contestant|
+    age += (contestant["age"]).to_i
+    contestants += 1
+  end
+  (age/contestants.to_f).round(0)
+  end
